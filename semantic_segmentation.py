9c924a18b7877d8d0096f786c46a75969f971667
Lucien Hinderling <lucien.hinderling@unifr.ch>
2020-11-08 00:34:19 +0100

    removed trad. filterbanks, added scaled vgg16 filters

--- semantic_segmentation.py
+++ semantic_segmentation.py
@@ -1,138 +1,31 @@
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import MinMaxScaler
-import numpy as np
-import mahotas
 import cv2
-import os
-import h5py
+import gc
 import glob
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.ensemble import RandomForestClassifier
-from skimage import io
-from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Conv2D
-import os
-from tensorflow.keras.applications.vgg16 import VGG16
 import multiprocessing
-import time
+import numpy as np
+import os
 import pickle
+import tensorflow as tf
+import time
 from scipy import ndimage as nd
 from skimage import exposure
 from skimage import io
-import gc
+from skimage import io
+from skimage.transform import resize
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.models import Sequential, Model
 
-def fd_VGG16(img,model):
-    input_image_stacked = np.expand_dims(img, axis=-1) 
-               #model preparation
-    
-    #new_model.summary()
-    #as it works only with 3 input channels: stack nuclear channel
-    stacked_img = np.stack((img,)*3, axis=2)
-    stacked_img = np.squeeze(stacked_img)
-    stacked_img = stacked_img.astype(np.float32)
-    stacked_img = stacked_img.reshape(-1, 1024, 1024, 3)
-    
-    features=model.predict(stacked_img)
-    
-    fv_VGG16= np.squeeze(features)
-    return fv_VGG16
-
-def init_VGG16():
-    VGG_model = VGG16(weights='imagenet', include_top=False, input_shape=(1024, 1024, 3))
-
-    #diable training (use pretrained weights)
-    for layer in VGG_model.layers:
-        layer.trainable = False
-
-
-    #only use up to last layer where input size is still 1024x1024
-    new_model = Model(inputs=VGG_model.input, outputs=VGG_model.get_layer('block1_conv2').output)
-    VGG_model = []
-    return new_model
 
 
 #https://github.com/87surendra/Random-Forest-Image-Classification-using-Python/blob/master/Random-Forest-Image-Classification-using-Python.ipynb
 #https://github.com/bnsreenu/python_for_microscopists/blob/master/062-066-ML_06_04_TRAIN_ML_segmentation_All_filters_RForest.py
 
-def fd_blur(image):
-    kernel = np.ones((5,5),np.float32)/25
-    dst = cv2.filter2D(image,-1,kernel)
-    #print(np.shape(dst))
-    dst = np.expand_dims(dst,axis=2)
-    return dst
 
-
-def fd_gabor(image):
-    #Generate Gabor features
-    num = 1  #To count numbers up in order to give Gabor features a lable in the data frame
-    kernels = []
-    
-    for theta in range(2):   #Define number of thetas
-        theta = theta / 4. * np.pi
-        for sigma in (1, 3):  #Sigma with 1 and 3
-            for lamda in np.arange(0, np.pi, np.pi / 4):   #Range of wavelengths
-                for gamma in (0.05, 0.5):   #Gamma values of 0.05 and 0.5
-                    gabor_label = 'Gabor' + str(num)  #Label Gabor columns as Gabor1, Gabor2, etc.
-    #                print(gabor_label)
-                    ksize=9
-                    kernel = cv2.getGaborKernel((ksize, ksize), sigma, theta, lamda, gamma, 0, ktype=cv2.CV_32F)    
-                    kernels.append(kernel)
-                    #Now filter the image and add values to a new column 
-                    fimg = cv2.filter2D(image, cv2.CV_8UC3, kernel)
-                    fimg = np.expand_dims(fimg, axis=2)
-                    if num == 1:
-                        filtered_img = fimg#fimg.reshape(-1)
-                    else:
-                        filtered_img = np.concatenate((filtered_img,fimg),axis=2)
-                    num += 1  #Increment for gabor column label
-    return filtered_img
-
-def fd_filter_collection(img):
-    
-        #CANNY EDGE
-        #TODO: change min/max
-    edges = cv2.Canny(img.astype(np.uint8), 100,200)   #Image, min and max values
-    edges = np.expand_dims(edges, axis=2)
-    
-    from skimage.filters import roberts, sobel, scharr, prewitt
-
-    #ROBERTS EDGE
-    edge_roberts = roberts(img)
-    edge_roberts = np.expand_dims(edge_roberts, axis=2)
-
-    #SOBEL
-    edge_sobel = sobel(img)
-    edge_sobel = np.expand_dims(edge_sobel, axis=2)
-
-    #SCHARR
-    edge_scharr = scharr(img)
-    edge_scharr = np.expand_dims(edge_scharr, axis=2)
-
-    #PREWITT
-    edge_prewitt = prewitt(img)
-    edge_prewitt = np.expand_dims(edge_prewitt, axis=2)
-
-    #GAUSSIAN with sigma=3
-    from scipy import ndimage as nd
-    gaussian_img = nd.gaussian_filter(img, sigma=3)
-    gaussian_img = np.expand_dims(gaussian_img, axis=2)
-
-    #GAUSSIAN with sigma=7
-    gaussian_img2 = nd.gaussian_filter(img, sigma=7)
-    gaussian_img2 = np.expand_dims(gaussian_img2, axis=2)
-
-    #MEDIAN with sigma=3
-    median_img = nd.median_filter(img, size=3)
-    median_img = np.expand_dims(median_img, axis=2)
-
-    #VARIANCE with size=3
-    variance_img = nd.generic_filter(img, np.var, size=3)
-    variance_img = np.expand_dims(variance_img, axis=2)
-    filters = [edges,edge_roberts,edge_sobel,edge_scharr,edge_prewitt,gaussian_img,gaussian_img2,median_img,variance_img]
-    filters_stacked = np.stack(filters,axis=2)
-    return filters_stacked
-    
 def annotations_to_tensor(feature_matrix,mask):
     #feature matrix dim: [x,y,nb_features]
     #possible mask elements: NaN: not annotated, int[0,1]: class annotation
@@ -145,32 +38,23 @@ def annotations_to_tensor(feature_matrix,mask):
     X_features = np.asarray(X_features)
     return X_features,y_labels
 
-def extract_features(input_image,model_VGG16):
-    #extract features from an image, and stack them
-    pixel_value = np.expand_dims(input_image, axis=2)
-    fv_blur = fd_blur(input_image)
-    fv_gabor = fd_gabor(input_image)
-    fv_filter_collection = fd_filter_collection(input_image)
-    fv_filter_collection = np.squeeze(fv_filter_collection)
-    fv_VGG16 = fd_VGG16(input_image, model_VGG16)
-    global_feature = np.concatenate([pixel_value, fv_blur,fv_gabor,fv_filter_collection,fv_VGG16],axis=2)
-    
-    #scaler = MinMaxScaler(feature_range=(0, 1))
-    #rescaled_features = scaler.fit_transform(global_features)
-    #print("shape of feature layer: "+str(np.shape(global_feature)))
-    return global_feature
 
 
 def apply_clf(args):
     path, clf = args[0],args[1]
     frames = np.load(path)
     output_stack = []
-    model_VGG16 = init_VGG16()
+    
+    
+    shapes = [(1024,1024),(512,512),(256,256)]
+    models = init_VGG16_pyramid(shapes)
+    
     for frame_nb in (range(np.shape(frames)[0])):
         frame = frames[frame_nb,:,:]
-        feature_image = extract_features(frame,model_VGG16)
-        feature_vector = feature_image.reshape(np.shape(feature_image)[0]*np.shape(feature_image)[1],np.shape(feature_image)[2])
-        prediction_vector = clf.predict_proba(feature_vector)[:,0] #[:,] to only use the prob. of the first class
+        features = fd_VGG16_pyramid(frame,models,shapes)
+        to_predict = features.reshape(np.shape(features)[0]*np.shape(features)[1],np.shape(features)[2])
+        
+        prediction_vector = clf.predict_proba(to_predict)[:,0] #[:,] to only use the prob. of the first class
         prediction_image = np.reshape(prediction_vector, (1024, 1024))
         output_stack.append(prediction_image)
         
@@ -178,14 +62,12 @@ def apply_clf(args):
         feature_vector = []
         prediction_vector = []
         prediction_image = []
-        gc.collect()
         
     output_stack = np.array(output_stack)
     output = output_stack*255
     output = output.astype('uint8')
     
     return output
-
 
 
 
@@ -223,15 +105,75 @@ def apply_semantic_segmentation(chunk_paths, model):
     time_total =  time_stop - time_start
     print("Semantic segmentation done. Processing time per frame: "+str(round(time_total/nb_frames, 2) )+" seconds. Total time: "+str(round(time_total/60,2))+" minutes.") 
     return stack_array
+
+
+def fd_VGG16_scaled(img,model,shape = (1024, 1024)):
+    
+    #resize image to new shape  
+    input_image_stacked = np.expand_dims(img, axis=-1)
+    if shape != (1024, 1024):
+        img = tf.image.resize(input_image_stacked,shape)
+    #new_model.summary()
+    #as it works only with 3 input channels: stack nuclear channel
+    stacked_img = np.stack((img,)*3, axis=2)
+    stacked_img = np.squeeze(stacked_img)
+    stacked_img = stacked_img.astype(np.float32)
+    
+    stacked_img = stacked_img.reshape(-1, shape[0], shape[1], 3)
+    
+    #predict class in keras for each pixel
+    features=model.predict(stacked_img)
+    
+    #remove extra dim
+    fv_VGG16= np.squeeze(features)
+    
+    #scale up to match original img size
+    #fv_VGG16 = resize(fv_VGG16,(1024,1024))
+    if shape!= (1024, 1024):
+        fv_VGG16 = tf.image.resize(fv_VGG16,(1024,1024))
+    return fv_VGG16
+
+
+def fd_VGG16_pyramid(img,models,shapes):
+    #img - input image to calculate vgg response of
+    #models - list of all vgg16 models 
+    #shapes - corresponding shapes
+    
+    fv_list = []
+    for model,shape in zip(models,shapes):
+        fv = fd_VGG16_scaled(img,model,shape)
+        fv_list.append(fv)
+    
+    global_feature = np.concatenate(fv_list,axis=2)
+    return global_feature    
+    
     
 
-def interface(input_image, classifier, alpha):
+def init_VGG16_pyramid(input_shapes=[(1024, 1024)]):
+    models = []
+    for shape in input_shapes:
+        keras_shape = (shape[0],shape[1],3) #add color channel
+        VGG_model = VGG16(weights='imagenet', include_top=False, input_shape=keras_shape)
+        #VGG_model.summary()
+
+        #disable training (use pretrained weights)
+        for layer in VGG_model.layers:
+            layer.trainable = False
+
+        #only use up to last layer where input size is still 1024x1024
+        new_model = Model(inputs=VGG_model.input, outputs=VGG_model.get_layer('block1_conv2').output)
+        models.append(new_model)
+    return models
+
+
+def interface(input_image, classifier, alpha,mask = None):
     drawing = False # true if mouse is pressed
     ix,iy = -1,-1
     mode = True
     cv2.destroyAllWindows() #close any windows left 
     #convert 16bit-1channel input image into grayscale uint8 3channel image
     #rescale the intensity so we can see more
+    
     p2, p98 = np.percentile(input_image, (2, 98))
     img8 = exposure.rescale_intensity(input_image, in_range=(p2, p98))
     scaler = MinMaxScaler(feature_range=(0, 255))
@@ -241,18 +183,24 @@ def interface(input_image, classifier, alpha):
     img8 = img8.astype('uint8')
     img8 = np.stack((img8,)*3, axis=-1)
 
-   
     
+    shapes = [(1024,1024),(512,512),(256,256)]
+    models = init_VGG16_pyramid(shapes)
+    features = fd_VGG16_pyramid(input_image,models,shapes)
+    
+    radius = 2
+    
+    print(features.shape)
     def draw_circle(event,x,y,flags,param):
         if event == cv2.EVENT_LBUTTONDOWN:
             drawing = True
             ix,iy = x,y
             if mode == True:
-                cv2.circle(output,(x,y),2,(255,100,100),-1)
-                cv2.circle(mask_one_channel,(x,y),2,(0))
+                cv2.circle(output,(x,y),radius,(255,100,100),-1)
+                cv2.circle(mask_one_channel,(x,y),radius,(0))
             else:
-                cv2.circle(output,(x,y),2,(100,255,100),-1)
-                cv2.circle(mask_one_channel,(x,y),2,(1))
+                cv2.circle(output,(x,y),radius,(100,255,100),-1)
+                cv2.circle(mask_one_channel,(x,y),radius,(1))
         elif event == cv2.EVENT_LBUTTONUP:
             drawing = False
 
@@ -264,11 +212,15 @@ def interface(input_image, classifier, alpha):
     dest = np.empty(np.shape(background),np.uint8)
     dest = dest.astype('uint8')
     update = False
-    model_VGG16 = init_VGG16()
-    features = extract_features(input_image,model_VGG16)
-    reuse_mask = False
-    if reuse_mask:
+    
+    
+    shape = (1024,1024)
+    #model_VGG16 = init_VGG16_scaled(shape)
+    #features = extract_features(input_image,model_VGG16)
+    #features = fd_VGG16_scaled(input_image,model_VGG16,shape)
+    if type(mask) != type(None):
         no_mask_initialized = False
+        mask_one_channel = mask
         
     else:
         no_mask_initialized = True
@@ -309,6 +261,7 @@ def interface(input_image, classifier, alpha):
                 print('\r' + 'Click to label [  NUCLEI  ]', end='')
                 
         k = cv2.waitKey(1) & 0xFF
+        
         if k == ord('m'):
             mode = not mode
             if mode:
@@ -317,8 +270,12 @@ def interface(input_image, classifier, alpha):
                 print('\r' + 'Click to label [  NUCLEI  ]', end='')
         elif k == ord('q'):
             break
-        elif k == ord('u'): #update
-            update = True       
+        elif k == ord('u'):
+            update = True   
+        elif k in list(map(ord,list(map(str,range(0,10))))):
+            #change radius of pen to number key clicked
+            radius = int(chr(k))      
+            
 
     cv2.destroyAllWindows()
     labels = mask_one_channel 
